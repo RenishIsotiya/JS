0588fef9e8a5ae86dc5dfc69dd0e972ec1269a03
Renish_Isotiya <isotiyarenish2021@gmail.com>
2025-07-01 10:33:57 +0000

    CoversionOf DataType

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33sdd" => NaN
+//  true => 1 ; false => 0
+
+let isLoggedIn  = "Renish"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => False
+// "" => false
+//"Renish" => true
+// null => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
